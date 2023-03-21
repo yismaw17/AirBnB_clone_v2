a9d95d029bab08dffc4b970c995db684954882b3
leulkashay <leulkashay2012@gmail.com>
2023-03-20 18:25:56 -0700

    new update

--- models/user.py
+++ models/user.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""
-    module containing user class
-    module containing user class
-"""
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine, Column, Integer, String
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-        User class for the user
-        User class for the user
-    """
-    if (storage_engine == 'db'):
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
